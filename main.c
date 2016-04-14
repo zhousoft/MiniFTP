79ad708416b3507c75a8f1c3c2b50cd6e0a6e77c
zhou ubuntu <zhouzhongtaosoft@163.com>
2016-04-14 20:39:50 +0800

    完成建立服务器接收连接

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "common.h"
+#include "sysutil.h"
+#include "session.h"
+
 int main()
 {
 	if(getuid() != 0)//如果不是root用户启动
@@ -6,5 +9,42 @@ int main()
 		fprintf(stderr,"miniftpd: must be started ad root\n");
 		exit(EXIT_FAILURE);
 	}
+	
+	session_t sess = 
+	{
+		/*控制连接*/
+		-1,"","","",
+		/*父子进程通道*/
+		-1,-1,
+	};
+	int listenfd = tcp_server(NULL, 5189);/*创建服务器*/
+	int conn;
+	pid_t pid;
+
+	while(1)
+	{
+		/*接受客户端连接*/
+		conn = accept_timeout(listenfd, NULL, 0);
+		if(conn == -1)
+			ERR_EXIT("accept_timeout");
+
+		/*获得连接套接字后开辟新进程进行处理*/
+		pid = fork();
+		if(pid == -1)
+			ERR_EXIT("fork");
+
+		if(pid == 0)
+		{
+			close(listenfd);/*子进程不需要继续监听，关闭子进程的监听套接字*/
+			sess.ctrl_fd = conn;
+			begin_session(&sess);/*开启会话-包含两个进程（nobody进程，服务进程）*/
+		}
+		else
+		{
+			close(conn);/*父进程（主进程）不处理客户端连接，交给子进程处理*/
+		}
+
+	}
+	
 	return 0;
 }

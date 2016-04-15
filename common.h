5a428cc1bb15cf96729ae94ed625072934bf810c
zhou ubuntu <zhouzhongtaosoft@163.com>
2016-04-15 18:38:02 +0800

    更新头文件，添加配置文件路径宏

--- common.h
+++ common.h
@@ -16,6 +16,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 
 #define ERR_EXIT(m)\
 	do\
@@ -26,10 +28,10 @@
 	while(0)
 
 
-#define MAX_COMMAND_LINE 1024
+#define MAX_COMMAND_LINE 1024 //客户端发送的一行命令最大长度
 #define MAX_COMMAND 32
-#define MAX_ARG 1024 //参数最大值
-
+#define MAX_ARG 1024 //参数最大长度
+#define MINIFTP_CONF "miniftpd.conf"//配置文件路径
 
 
 #endif /*_COMMON_H_*/

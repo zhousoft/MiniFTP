57b8440851891eee4fedb5e9acc0778580a1dc0d
zhou ubuntu <zhouzhongtaosoft@163.com>
2016-04-21 17:23:11 +0800

    添加FTP状态码宏定义

---
+++ ftpcodes.h
@@ -0,0 +1,14 @@
+#ifndef _FTP_CODES_H_
+#define _FTP_CODES_H_
+
+
+#define FTP_DATACONN	150
+
+#define FTP_NOOPOK		200
+#define FTP_TYPEOK		200
+#define FTP_GREET		220
+#define FTP_LOGINOK		230
+#define FTP_GIVEPWORD	331
+#define FTP_LOGINERR	530
+
+#endif

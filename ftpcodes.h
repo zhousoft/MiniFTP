5fabf0400b88bb227eb2d80e4daf270b0f81dff0
zhou ubuntu <zhouzhongtaosoft@163.com>
2016-04-22 16:34:17 +0800

    增加状态码

--- ftpcodes.h
+++ ftpcodes.h
@@ -2,13 +2,18 @@
 #define _FTP_CODES_H_
 
 
-#define FTP_DATACONN	150
+#define FTP_DATACONN		150
 
-#define FTP_NOOPOK		200
-#define FTP_TYPEOK		200
-#define FTP_GREET		220
-#define FTP_LOGINOK		230
-#define FTP_GIVEPWORD	331
-#define FTP_LOGINERR	530
+#define FTP_NOOPOK			200
+#define FTP_TYPEOK			200
+#define FTP_FEAT			211
+#define FTP_SYSTOK			215
+#define FTP_GREET			220
+#define FTP_LOGINOK			230
+#define FTP_PWDOK			257
+#define FTP_GIVEPWORD		331
+#define FTP_BADCMD			500
+#define FTP_COMMANDNOTIMPL	502
+#define FTP_LOGINERR		530
 
 #endif
